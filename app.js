cd0d459c9671ac49aee841011363aa7cb9fbff89
Taylor <taylorkkaufman@gmail.com>
2023-03-14 22:18:06 -0600

    add v1 of color script

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+  }
+
+
+const letters = document.querySelectorAll('.letter');
+for (let letter of letters) {
+  letter.style.color = randomRGB();
+  setInterval(function () {
+    letter.style.color = randomRGB()
+  }, 1000);
+}
